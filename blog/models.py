9a38b1cb3720fbbe2a5a3761aa0b5e5f0411f7af
katherine95 <kathiekim95@gmail.com>
2017-06-12 03:52:42 +0300

    resume/webapp/blog using django framework

---
+++ blog/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    body =models.TextField(max_length=350)
+    date =models.DateTimeField()
+
+    def __str__(self):
+        return self.title
